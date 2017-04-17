e3a91d6ca3c3d6afdf904e521646789f9f04c85f
Keith <wilsonhung@HungLings-MacBook-Air.local>
2017-04-17 22:29:33 +0800

    added edit function

--- resources/main.js
+++ resources/main.js
@@ -3,7 +3,6 @@ var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem(
   completed:[]
 };
 
-
 //SVG buttons
 var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
 var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
@@ -167,59 +166,90 @@ function updateTodoCount(){
 
 function editItem() {
   var defaultText = $(this).parent().parent().clone().children().remove().end().text();
-  var editText = prompt("Modify a reminder...", defaultText);
   var itemNode = this.parentNode.parentNode;
   var id = itemNode.parentNode.id;
 
-  // If text is edited, update arrays 'todo' and 'completed'
-  if (editText){
-    if (id === 'todo'){
-      for ( var i = 0; i < data.todo.length; i++) {
-        if (data.todo[i] == defaultText){
-          data.todo[i] = editText;
-        }
-      }
-    }
-    else {
-      for ( var j = 0; j < data.completed.length; j++) {
-        if (data.completed[j] == defaultText){
-          data.completed[j] = editText;
-        }
-      }
-    }
-    // Update data to localStorage
-    dataObjectUpdated();
-    // Replace old text with newly inputted text in DOM
+  // Open modal
+  var modal = document.getElementById('modal');
+  modal.style.display = 'block';
+
+  // Set default text in input field
+  var input = document.getElementById('modalInput')
+  input.value = defaultText;
+  input.focus();
+
+  // Add click event to ok button
+  var ok = document.getElementById('save');
+  ok.addEventListener('click', function (){
+    var editText = document.getElementById('modalInput').value;
     itemNode.innerText = editText;
+
+    // Append the buttons back to the list in DOM
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+
+    // Add remove button
+    var remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = removeSVG;
+    // Add click event for removing item
+    remove.addEventListener('click', removeItem);
+
+    // Add complete button
+    var complete = document.createElement('button');
+    complete.classList.add('complete');
+    complete.innerHTML = completeSVG;
+    //Add click event for completing item
+    complete.addEventListener('click',completeItem)
+
+    // Add edit button
+    var edit = document.createElement('button');
+    edit.classList.add('edit');
+    edit.innerText = 'edit';
+    //Add edit event for editing item
+    edit.addEventListener('click', editItem)
+
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
+    buttons.appendChild(edit);
+    itemNode.appendChild(buttons);
+    closeModal();
+
+    // Update data.todo and data.compelted arrays
+    if (defaultText !== editText){
+      if (id === 'todo'){
+        for (var i = 0; i < data.todo.length; i++) {
+          if (data.todo[i] == defaultText) {
+            data.todo[i] = editText;
+          }
+        }
+      }
+      else {
+        for (var j=0; j<data.completed.length; j++){
+          if (data.completed[j] == defaultText) {
+            data.completed[j] = editText;
+          }
+        }
+      }
+      // Update data to localStorage
+      dataObjectUpdated();
+    }
+  });
+
+  // Add click event to cancel button
+  var cancel = document.getElementById('cancel');
+  cancel.addEventListener('click', closeModal);
+  // Allows users to close modal by clicking on grey area
+  window.addEventListener('click', exitModal);
 }
-  // Append the buttons back to the list in DOM
-  var buttons = document.createElement('div');
-  buttons.classList.add('buttons');
 
-  // Add remove button
-  var remove = document.createElement('button');
-  remove.classList.add('remove');
-  remove.innerHTML = removeSVG;
-  // Add click event for removing item
-  remove.addEventListener('click', removeItem);
+function closeModal() {
+  var modal = document.getElementById('modal');
+  modal.style.display = 'none';
+}
 
-  // Add complete button
-  var complete = document.createElement('button');
-  complete.classList.add('complete');
-  complete.innerHTML = completeSVG;
-  //Add click event for completing item
-  complete.addEventListener('click',completeItem)
-
-  // Add edit button
-  var edit = document.createElement('button');
-  edit.classList.add('edit');
-  edit.innerText = 'edit';
-  //Add edit event for editing item
-  edit.addEventListener('click', editItem)
-
-  buttons.appendChild(remove);
-  buttons.appendChild(complete);
-  buttons.appendChild(edit);
-  itemNode.appendChild(buttons);
-
+function exitModal(event){
+  if(event.target == modal) {
+    modal.style.display='none';
+  }
 }
